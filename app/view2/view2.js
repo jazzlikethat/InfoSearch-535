20a641d827fc6da88c7e5af4faeb545bdb7193e2
Pradeep Kumar Aitha <paitha@buffalo.edu>
2018-12-09 18:31:38 -0500

    added filters

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,13 +9,18 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', ['$scope', '$location', '$window', '$http', 'PagerService', 'sharedData', function($scope, $location, $window, $http, PagerService, sharedData) {
+.controller('View2Ctrl', ['$scope', '$location', '$window', '$http', 'PagerService', 'sharedData', '$timeout', function($scope, $location, $window, $http, PagerService, sharedData, $timeout) {
     
     $scope.searchInput = $location.search().query;
     $scope.searchResults = [];
     $scope.slicedResults = [];
     $scope.pager = {};
     $scope.hideSpinner = true;
+    $scope.filters = {
+      language: null,
+      city: null,
+      date: null
+    };
     
     $scope.goToHomePage = goToHomePage;
     $scope.fetchResultsForQuery = fetchTweets;
@@ -23,16 +28,24 @@ angular.module('myApp.view2', ['ngRoute'])
     $scope.openUrl = openUrl;
     $scope.setPage = setPage;
     $scope.gotoUser = gotoUser;
+    $scope.applyFilters = applyFilters;
+    $scope.clearFilters = clearFilters;
 
     var script_ip = "18.188.21.89";
     var script_port = "5000";
+    var tempTweets = [];
 
     // On load
     fetchTweets();
 
     function fetchTweets(){
+      if ($scope.searchInput.trim() === '') {
+        return;
+      }
       $scope.pager = {};
       $scope.hideSpinner = false;
+      $scope.filters.language = null;
+      $scope.filters.city = null;
       $http({
         url: "http://" + script_ip + ":" + script_port + "/query/" + $scope.searchInput,
         method: "GET",
@@ -42,6 +55,7 @@ angular.module('myApp.view2', ['ngRoute'])
         $scope.searchResults = response.data;
         sharedData.searchInput = $scope.searchInput;
         sharedData.searchResults = $scope.searchResults;
+        tempTweets = angular.copy($scope.searchResults.tweets);
         $scope.setPage(1);
         $scope.hideSpinner = true;
       });
@@ -77,6 +91,40 @@ angular.module('myApp.view2', ['ngRoute'])
       $scope.slicedResults = $scope.searchResults.tweets.slice($scope.pager.startIndex, $scope.pager.endIndex + 1);
       
       $window.scrollTo(0, 0);
+    }
+
+    function applyFilters() {
+      $scope.pager = {};
+      $scope.hideSpinner = false;
+      $scope.searchResults.tweets = angular.copy(tempTweets);
+      if ($scope.filters.language) {
+        var tweets = $scope.searchResults.tweets;
+        $scope.searchResults.tweets = tweets.filter(function(tweet){
+          return tweet.tweet_lang[0] === $scope.filters.language;
+        });
+      }
+      if ($scope.filters.city) {
+        var tweets = $scope.searchResults.tweets;
+        $scope.searchResults.tweets = tweets.filter(function(tweet){
+          return tweet.city[0] === $scope.filters.city;
+        })
+      }
+      $scope.setPage(1);
+      $timeout(function(){
+        $scope.hideSpinner = true;
+      }, 500);
+    }
+
+    function clearFilters() {
+      $scope.hideSpinner = false;
+      $scope.filters.language = null;
+      $scope.filters.city = null;
+      $scope.pager = {};
+      $scope.searchResults.tweets = angular.copy(tempTweets);
+      $scope.setPage(1);
+      $timeout(function(){
+        $scope.hideSpinner = true;
+      }, 500);
     }
     
 }]);
