b7f00c3330e5ee5fe86e79d52d8d26232d572aa3
Pradeep Kumar Aitha <paitha@buffalo.edu>
2018-12-09 22:50:38 -0500

    added wordcloud

--- app/view3/view3.js
+++ app/view3/view3.js
@@ -143,9 +143,49 @@ angular.module('myApp.view3', ['ngRoute'])
           var lang = entry.tweet_lang[0];
           text += entry['text_' + lang];
         }
-        debugger;
         return text;
       }
+
+      var myConfig = {
+        type: 'wordcloud',
+        options: {
+          text: completeTweetText,
+          minLength: 5,
+          maxItems: 40,
+          aspect: 'flow-center',
+          rotate: true,
+          colorType: 'palette',
+          palette: ['#D32F2F','#5D4037','#1976D2','#E53935','#6D4C41','#1E88E5','#F44336','#795548','#2196F3','#EF5350','#8D6E63','#42A5F5'],
+        
+          style: {
+            fontFamily: 'Crete Round',
+          
+            hoverState: {
+              backgroundColor: '#D32F2F',
+              borderRadius: 2,
+              fontColor: 'white'
+            },
+            tooltip: {
+              text: '%text: %hits',
+              visible: true,
+              
+              alpha: 0.9,
+              backgroundColor: '#1976D2',
+              borderRadius: 2,
+              borderColor: 'none',
+              fontColor: 'white',
+              fontFamily: 'Georgia',
+              textAlpha: 1
+            }
+          }
+        }
+      }
+
+      zingchart.render({ 
+        id: 'chart_div4', 
+        data: myConfig, 
+        height: 400
+      });
  
 }]);
 
